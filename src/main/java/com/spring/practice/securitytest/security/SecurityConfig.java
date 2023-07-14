06b7b69e85c817ef03ec185634937f23d2b8c793
Kulebyakin Pavel <pavel.kulebyakin.study@gmail.com>
2023-07-15 02:41:37 +0300

    Add SecurityConfig

--- src/main/java/com/spring/practice/securitytest/security/SecurityConfig.java
+++ src/main/java/com/spring/practice/securitytest/security/SecurityConfig.java
@@ -4,10 +4,12 @@ import com.spring.practice.securitytest.repository.UserRepository;
 import com.spring.practice.securitytest.user.User;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -24,5 +26,20 @@ public class SecurityConfig {
             if (user != null) return user;
             throw new UsernameNotFoundException("User ‘" + username + "’ not found");
         };
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeHttpRequests(authorise -> authorise
+                        .requestMatchers("/login/**", "/register/**", "/css/**", "").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(login -> login
+                        .loginPage("/login")
+                        .failureUrl("/login?error=true"))
+                .oauth2Login(oauth2 -> oauth2.loginPage("/login"))
+                .logout(logout -> logout.logoutSuccessUrl("/login"));
+        return httpSecurity.build();
     }
 }
