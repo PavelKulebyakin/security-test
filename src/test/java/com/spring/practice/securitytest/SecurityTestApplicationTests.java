0eeaa8017e00aff32d95c69364c0fa84d53f0ce0
Kulebyakin Pavel <pavel.kulebyakin.study@gmail.com>
2023-07-14 13:56:38 +0300

    Initial commit

---
+++ src/test/java/com/spring/practice/securitytest/SecurityTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.practice.securitytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
