3acc7047d0be9dce672206a42d800d44a1ec8b8e
Kulebyakin Pavel <pavel.kulebyakin.study@gmail.com>
2023-07-15 02:42:48 +0300

    Change Spring app file

--- src/main/java/com/spring/practice/securitytest/SecurityTestApplication.java
+++ src/main/java/com/spring/practice/securitytest/SecurityTestApplication.java
@@ -1,7 +1,13 @@
 package com.spring.practice.securitytest;
 
+import com.spring.practice.securitytest.repository.UserRepository;
+import com.spring.practice.securitytest.user.RegistrationForm;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class SecurityTestApplication {
@@ -10,4 +16,13 @@ public class SecurityTestApplication {
         SpringApplication.run(SecurityTestApplication.class, args);
     }
 
+    @Bean
+    @Profile("test")
+    public CommandLineRunner dataLoader(
+            UserRepository repository,
+            PasswordEncoder encoder) {
+        return args -> {
+            repository.save(new RegistrationForm("user", "mypas", "Pavel").toUser(encoder));
+        };
+    }
 }
